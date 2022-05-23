efc0d51b09d2582ea732da3db549d268af3b7a4f
Kamesh jayapalan <kameshjayapalan90@gmail.com>
2022-05-23 11:08:00 +0530

    Initial Commit

---
+++ TodoListApp/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TodoListApp
+{
+    public class TodoItem
+    {
+        public String TodoText
+        {
+            get;set;
+        }
+        public bool Complete
+        {
+            get; set;
+        }
+        public TodoItem(String TodoText,bool Complete)
+        {
+            this.TodoText = TodoText;
+            this.Complete = Complete;
+        }
+    }
+}
---
+++ TodoListApp/TodoListViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace TodoListApp
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems
+        {
+            get;set;
+        }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem(" todo1",true));
+            TodoItems.Add(new TodoItem(" todo2", false));
+            TodoItems.Add(new TodoItem(" todo3", false));
+
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+
+        public String NewTodoInputValue { get; set; }
+
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+
+
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItem = o as TodoItem;
+            TodoItems.Remove(todoItem);
+
+        }
+    }
+}
